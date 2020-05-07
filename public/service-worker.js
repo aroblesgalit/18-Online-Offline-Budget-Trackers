741a3af143e469d80b401f325d51953ef202b1da
Alvin Galit <aroblesgalit@gmail.com>
2020-05-07 17:51:25 -0500

    Set up cache files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,14 @@
 console.log("Hello from service worker!");
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/index.js",
+    "/manifest.webmanifest",
+    "/style.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
