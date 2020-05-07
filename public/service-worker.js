4091036690403c4f6764f06e67582debaed492ef
Alvin Galit <aroblesgalit@gmail.com>
2020-05-07 18:54:22 -0500

    Installed and registered a service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
 console.log("Hello from service worker!");
 
+// Set up cache files
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
@@ -12,3 +13,15 @@ const FILES_TO_CACHE = [
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+// Install and register service worker
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log("Your files were pre-cached successfully!");
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    self.skipWaiting();
+})
